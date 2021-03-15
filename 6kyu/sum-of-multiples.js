7d21f7e9114ce5dc1e98dfed8067364c813cf443
“Elvia <elvia.sicuro@gmail.com>
2021-03-15 20:43:42 +0100

    multiple of 5 and 3 solved

---
+++ 6kyu/sum-of-multiples.js
@@ -0,0 +1,22 @@
+
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+//Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+//Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them)
+
+function solution(number){
+    let array = []
+    for (let i=0; i<number; i++) {
+      //console.log(i % 3)
+      if(i % 3 === 0 || i % 5 === 0) {
+        array.push(i)
+      }
+      //console.log(array)
+    }
+  let sum= array.reduce((acc, val) => acc+val,0)
+  return sum
+  }
+
+// if i modulo 3 or 5 is 0, then push it into the array 
+//sum up all the elements in the array
